3d06977599067b54095d1f64c2777f5d5caf75dc
RohitRajP <rij7u7d@gmail.com>
2019-05-02 13:43:28 +0530

    Added much more functionality to task scheduler

--- lib/pages/schedulePage.dart
+++ lib/pages/schedulePage.dart
@@ -15,14 +15,51 @@ class SchedulePage extends StatefulWidget {
 }
 
 class _SchedulePageState extends State<SchedulePage> {
-  List<String> _scheduleList;
+  List<String> _scheduleList, _permanentList;
   GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>();
+  TextEditingController _taskController = new TextEditingController();
+
   String _task;
   bool _isLoading = true;
+  final List<FloatingActionButton> fabs = [
+    new FloatingActionButton(
+      child: new Icon(Icons.access_time),
+      onPressed: () {},
+    ),
+    new FloatingActionButton(
+      child: new Icon(Icons.account_balance),
+      onPressed: () {},
+    ),
+    new FloatingActionButton(
+      child: new Icon(Icons.add_alert),
+      onPressed: () {},
+    )
+  ];
 
   void initState() {
+    // _dateTimeVerification();
     _getSchedule();
   }
+
+  // void _dateTimeVerification() async {
+  //   SharedPreferences prefs = await SharedPreferences.getInstance();
+  //   print(prefs.getStringList('templateList'));
+  //   print(prefs.getStringList('scheduleList'));
+  //   print(_scheduleList);
+  //   String lastTime = prefs.getString('lastTime');
+  //   if (lastTime == null) {
+  //     prefs.setString('lastTime', DateTime.now().toString());
+  //   } else {
+  //     DateTime before = DateTime.parse(lastTime);
+  //     DateTime now = DateTime.now();
+  //     //print(before.difference(now).inHours);
+  //     if (before.difference(now).inHours > 24) {
+  //       _scheduleList = prefs.getStringList('templateList');
+  //       prefs.setStringList('scheduleList', _scheduleList);
+  //     }
+  //   }
+  //   _getSchedule();
+  // }
 
   void _getSchedule() async {
     setState(() {
@@ -30,8 +67,12 @@ class _SchedulePageState extends State<SchedulePage> {
     });
     SharedPreferences prefs = await SharedPreferences.getInstance();
     _scheduleList = prefs.getStringList('scheduleList');
+    _permanentList = prefs.getStringList('permanentList');
     if (_scheduleList == null) {
       _scheduleList = [];
+    }
+    if (_permanentList == null) {
+      _permanentList = [];
     }
     setState(() {
       _isLoading = false;
@@ -110,13 +151,25 @@ class _SchedulePageState extends State<SchedulePage> {
         });
   }
 
-  void _updateCreateSharedPrefs() async {
+  void _updateCreateSharedPrefs(String _task, int addOrNot) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     prefs.setStringList('scheduleList', _scheduleList);
+    _permanentList = prefs.getStringList('permanentList');
+    if (_permanentList == null) {
+      _permanentList = [];
+    } else {
+      _permanentList = prefs.getStringList('permanentList');
+    }
+    if (addOrNot == 1) {
+      _permanentList.add(_task);
+    }
+    prefs.setStringList('permanentList', _permanentList);
+    print(prefs.getStringList('permanentList'));
+    // prefs.setStringList('templateList', _scheduleList);
     //print(prefs.getStringList('scheduleList'));
   }
 
-  void _showCreateIdea() {
+  void _showCreateTask() {
     showDialog(
         context: context,
         builder: (BuildContext context) {
@@ -154,7 +207,8 @@ class _SchedulePageState extends State<SchedulePage> {
                 child: Text("Add Task"),
                 onPressed: () {
                   _scheduleList.add(_task);
-                  _updateCreateSharedPrefs();
+                  // _permanentList.add(_task);
+                  _updateCreateSharedPrefs(_task, 1);
                   Navigator.pop(context);
                 },
               )
@@ -163,7 +217,82 @@ class _SchedulePageState extends State<SchedulePage> {
         });
   }
 
-  void _deleteTask(int index) async {
+  void _updatePermanentList() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setStringList('permanentList', _permanentList);
+  }
+
+  Widget _allTasksList(BuildContext context, int index) {
+    return ExpansionTile(
+      title: Text(_permanentList[index]),
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            FlatButton.icon(
+              textColor: Colors.green,
+              icon: Icon(Icons.add),
+              label: Text("Add"),
+              onPressed: () {
+                _scheduleList.add(_permanentList[index]);
+                Navigator.pop(context);
+                setState(() {});
+              },
+            ),
+            FlatButton.icon(
+              textColor: Colors.red,
+              icon: Icon(Icons.delete),
+              label: Text("Delete"),
+              onPressed: () {
+                _permanentList.removeAt(index);
+                _updatePermanentList();
+                Navigator.pop(context);
+              },
+            )
+          ],
+        )
+      ],
+    );
+  }
+
+  void _showEditTasks() {
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)),
+            title: Text("Task List"),
+            content: SizedBox(
+              width: 400,
+              child: Container(
+                child: (_permanentList.length == 0)
+                    ? Center(
+                        child: Text("No data to show"),
+                      )
+                    : ListView.builder(
+                        itemBuilder: _allTasksList,
+                        itemCount: _permanentList.length,
+                      ),
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0)),
+                textColor: Colors.white,
+                color: Colors.green,
+                child: Text("Done"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ],
+          );
+        });
+  }
+
+  void _taskComplete(int index) async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     setState(() {
       _scheduleList.removeAt(index);
@@ -201,17 +330,96 @@ class _SchedulePageState extends State<SchedulePage> {
     prefs.setStringList('scheduleList', _scheduleList);
   }
 
+  void _showTaskEdit(index) {
+    _taskController.text = _scheduleList[index];
+    showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(5.0)),
+            title: Text("Edit Task"),
+            content: SizedBox(
+              width: 400,
+              child: Container(
+                child: TextField(
+                  controller: _taskController,
+                  onChanged: (value) {
+                    _task = value;
+                  },
+                  decoration: InputDecoration(labelText: "Edit this task"),
+                ),
+              ),
+            ),
+            actions: <Widget>[
+              FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0)),
+                textColor: Colors.white,
+                color: Colors.red,
+                child: Text("Cancel"),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              FlatButton(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(5.0)),
+                textColor: Colors.white,
+                color: Colors.green,
+                child: Text("Edit Task"),
+                onPressed: () {
+                  int flag = 0;
+                  for (int i = 0; i < _permanentList.length; i++) {
+                    if (_permanentList[i] == _scheduleList[index]) {
+                      _permanentList[i] = _task;
+                      flag++;
+                    }
+                  }
+                  _scheduleList[index] = _task;
+                  if (flag != 0) {
+                    _updateCreateSharedPrefs(_task, 0);
+                  } else {
+                    _updateCreateSharedPrefs(_task, 1);
+                  }
+                  // _permanentList.add(_task);
+
+                  Navigator.pop(context);
+                },
+              )
+            ],
+          );
+        });
+  }
+
   Widget _taskTile(BuildContext context, int index) {
     return Slidable(
         secondaryActions: <Widget>[
+          IconSlideAction(
+            caption: 'Edit',
+            foregroundColor: Colors.white,
+            color: Colors.purple,
+            icon: Icons.edit,
+            onTap: () {
+              _showTaskEdit(index);
+            },
+          ),
           IconSlideAction(
             caption: 'Completed',
             color: Colors.green,
             icon: Icons.check,
             onTap: () {
-              _deleteTask(index);
+              _taskComplete(index);
             },
           ),
+          // IconSlideAction(
+          //   caption: 'Delete Task',
+          //   color: Colors.red,
+          //   icon: Icons.delete,
+          //   onTap: () {
+          //     _deleteTask(index);
+          //   },
+          // )
         ],
         actions: <Widget>[
           IconSlideAction(
@@ -247,13 +455,31 @@ class _SchedulePageState extends State<SchedulePage> {
             onWillPop: _onWillPop,
             child: Scaffold(
               key: _scaffoldKey,
-              floatingActionButton: FloatingActionButton(
-                foregroundColor: Colors.white,
-                onPressed: () {
-                  _showCreateIdea();
-                },
-                backgroundColor: Colors.green,
-                child: Icon(FontAwesomeIcons.plus),
+              floatingActionButton: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: <Widget>[
+                  FloatingActionButton.extended(
+                    foregroundColor: Colors.white,
+                    onPressed: () {
+                      _showEditTasks();
+                    },
+                    backgroundColor: Colors.purple,
+                    icon: Icon(FontAwesomeIcons.list),
+                    label: Text("List"),
+                  ),
+                  SizedBox(
+                    width: 10.0,
+                  ),
+                  FloatingActionButton.extended(
+                    foregroundColor: Colors.white,
+                    onPressed: () {
+                      _showCreateTask();
+                    },
+                    backgroundColor: Colors.green,
+                    icon: Icon(FontAwesomeIcons.plus),
+                    label: Text('Create'),
+                  ),
+                ],
               ),
               appBar: AppBar(
                 title: Text("Daily Tasks"),
@@ -375,7 +601,7 @@ class _SchedulePageState extends State<SchedulePage> {
                             child: Container(
                               padding: EdgeInsets.all(20.0),
                               child: Text(
-                                '😮 \n There are no daily tasks to show right now \n\n How about you try creating one by pressing the + icon below \n 😃',
+                                '😮 \n There are no daily tasks to show right now \n How about you try creating one by pressing the + icon below \n 😃',
                                 textAlign: TextAlign.center,
                               ),
                             ),
